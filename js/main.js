bb45f9414ac699396cc884146e5e3c8e352e92d0
Alessio <ale79631@gmail.com>
2021-02-19 20:04:24 +0100

    correzione

--- js/main.js
+++ js/main.js
@@ -7,18 +7,25 @@ console.log(eta);
 chilometri = prompt("Inserisci i chilometri che vuoi percorrere.");
 console.log(chilometri);
 
-if (eta < 18) {
-    costo = chilometri * costante;
-    sconto = costo * 0.2;
-    costo = costo - sconto;
-    alert("Il costo del biglietto e' " + costo + " Euro.");
-    
-} else if (eta > 65) {
-    costo = chilometri * costante;
-    sconto = costo * 0.4;
-    costo = costo - sconto;
-    alert("Il costo del biglietto e' " + costo + " Euro.");
+if (isNaN(eta) || isNaN(chilometri)) {
+    alert('I valori inseriti non sono numerici. Prego, inserire esclusivamente valori numerici.');
 } else {
-    costo = chilometri * costante;
-    alert("Il costo del biglietto e' " + costo + " Euro.");
+    if (eta < 18) {
+        costo = chilometri * costante;
+        sconto = costo * 0.2;
+        costo = costo - sconto;
+        costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
+        alert("Il costo del biglietto e' " + costo + " Euro.");
+        
+    } else if (eta > 65) {
+        costo = chilometri * costante;
+        sconto = costo * 0.4;
+        costo = costo - sconto;
+        costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
+        alert("Il costo del biglietto e' " + costo + " Euro.");
+    } else {
+        costo = chilometri * costante;
+        costo = Math.round( costo * 100 + Number.EPSILON ) / 100;
+        alert("Il costo del biglietto e' " + costo + " Euro.");
+    }
 }
